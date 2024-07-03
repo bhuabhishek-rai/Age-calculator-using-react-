3febc7f4d279b8ed5bc448fcc16188ee7c3bb475
bhuabhishek-rai <bhuabhishek1998@gmail.com>
2024-07-03 12:49:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AgeCalculator from './componets/AgeCalculator';
+ 
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator/>
+    </div>
+  );
+}
+ 
+export default App;
